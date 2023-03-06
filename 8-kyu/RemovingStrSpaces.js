6092fc367dc334fa35afef807a7f6443079ad94f
Jessica Swenson <jessicamswens@gmail.com>
2023-03-06 07:56:25 -0700

    added removing strspaces

---
+++ 8-kyu/RemovingStrSpaces.js
@@ -0,0 +1,7 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+const noSpace = (str) => {
+    return str.replace(/\s/g, '')
+}
+
+console.log(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'));
