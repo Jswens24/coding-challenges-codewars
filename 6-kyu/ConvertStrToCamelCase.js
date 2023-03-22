65f6d1ab77bc5205c642dda60776a55bfa9291dd
Jessica Swenson <jessicamswens@gmail.com>
2023-03-22 07:22:23 -0600

    added convertstr

---
+++ 6-kyu/ConvertStrToCamelCase.js
@@ -0,0 +1,28 @@
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+// "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+
+const toCamelCase = (str) => {
+    const strSplitArr = str.split(/[-:_]/);
+
+    const mappedArr = strSplitArr.map((word, index) => {
+        if (index === 0) {
+            return word
+        } else {
+            return word[0].toUpperCase() + word.slice(1)
+        }
+    })
+
+    return joinStr = mappedArr.join('')
+
+}
+
+console.log(toCamelCase('the_stealth_warrior'))
+console.log(toCamelCase('The_Stealth_Warrior'))
+console.log(toCamelCase('the_stealth-warrior'))
