237b7010ef437ff9efb51912ccbf36cd0d800b6f
Jessica Swenson <jessicamswens@gmail.com>
2023-07-19 17:32:31 -0600

    added dm lab coding challenges

---
+++ dm-labs/maxOfTwo.js
@@ -0,0 +1,27 @@
+// Max of Two
+// Difficulty
+// Easy
+
+// Concepts
+// Mathematical Operators, Conditionals
+
+
+// Given the starting code of:
+
+// x = 1
+// y = 2
+// Write a block of code that will print out the largest value.
+
+// Using the given values as an example, your code should print out 2.
+
+// To verify your code is working properly, try and change the values of x and y.
+
+// As an additional challenge, print out The values are identical. if x and y have the same value.
+
+const maxOfTwo = (x, y) => x > y ? x : y
+
+
+console.log(maxOfTwo(1, 2))
+console.log(maxOfTwo(3, 3))
+console.log(maxOfTwo(14, 22))
+console.log(maxOfTwo(143, 214))
